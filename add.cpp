8b802ba48cd8c6fb9286919fc2e260bf4fbc9d7c
Megan <mkongable18@gmail.com>
2022-12-08 16:50:10 -0500

    everything

---
+++ add.cpp
@@ -0,0 +1,12 @@
+//program that adds two numbers and displays the result
+
+#include <iostream>
+
+int main()
+{
+    int a = 5;
+    int b = 2;
+    int c = a + b;
+    printf("The sum of %d and %d is %d", a, b, c);
+    return 0;
+}
---
+++ deckOfCards.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <functional>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <list>
+#include <array>
+#include <bitset>
+
+
+using namespace std;
+
+bool hasGroupsSizeX(vector<int>& deck) {
+    // try to partition the deck into groups of the same integer and size
+
+    // map integers to their frequency
+    unordered_map<int, int> freq;
+    for (int i = 0; i < deck.size(); i++) {
+        //check if deck[i] is in the map
+        if (freq.find(deck[i]) == freq.end()) {
+            //if not, add it
+            freq[deck[i]] = 1;
+        }
+        else {
+            //if so, increment the frequency
+            freq[deck[i]]++;
+        }
+    }
+
+    // find the greatest common divisor of all the frequencies
+    int gcd = 0;
+    for (auto it = freq.begin(); it != freq.end(); it++) {
+        gcd = __gcd(gcd, it->second);
+    }
+
+    // if the gcd is greater than 1, return true
+    if (gcd > 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main()
+{
+    return 0;
+}
