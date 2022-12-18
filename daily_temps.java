74c5898477f1ac22f5578eba014efd86d015112a
Kevin Li <54922253+kevinli22527@users.noreply.github.com>
2022-12-17 19:45:34 -0600

    comments for clarity

--- daily_temps.java
+++ daily_temps.java
@@ -3,7 +3,7 @@
         Stack<Integer> temp_stack = new Stack<>();
         int[] result = new int[temperatures.length];
         for (int i = 0; i < temperatures.length; i++) {
-            while (!temp_stack.isEmpty() && temperatures[i] > temp_stack.peek()) {
+            while (!temp_stack.isEmpty() && temperatures[i] > temp_stack.peek()) { //new temp incoming, while they are still cooler temperatures in the stack, pop them and update their indices in the result array to the index of this new incoming temp
                 result[index_stack.peek()] = i - index_stack.peek();
                 index_stack.pop();
                 temp_stack.pop();
