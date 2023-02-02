5a79b52b227302bf00274a2c746b982f9c97f279
Megan <mkongable18@gmail.com>
2023-02-01 19:30:48 -0500

    Daily Feb 2

---
+++ Jan2023/alienDictionary.py
@@ -0,0 +1,26 @@
+def isAlienSorted(self, words, order):
+    """
+    :type words: List[str]
+    :type order: str
+    :rtype: bool
+    """
+    # Determine if the words are sorted in the alien language
+    # Create a dictionary to map the order of the letters
+    orderDict = {}
+    for i in range(len(order)):
+        orderDict[order[i]] = i
+    # Compare the words
+    # if the first letters are different, make sure the first letter is in the correct order
+    # if the first letters are the same, compare the second letters
+    # if the second letters are different, make sure the second letter is in the correct order
+    # continue until the letter differs or the end of the word is reached
+    for i in range(len(words)-1):
+        for j in range(min(len(words[i]), len(words[i+1]))):
+            if words[i][j] != words[i+1][j]:
+                if orderDict[words[i][j]] > orderDict[words[i+1][j]]:
+                    return False
+                break
+        else:
+            if len(words[i]) > len(words[i+1]):
+                return False
+    return True
