1847349d12e5b4f48bc321f1c476714a1b6c8dfa
Kevin Li <54922253+kevinli22527@users.noreply.github.com>
2023-03-12 13:35:27 -0500

    mergeKLists

---
+++ March2023/mergeKLists.py
@@ -0,0 +1,17 @@
+def mergeKLists(self,lists:List[Optional[ListNode]])->Optional[ListNode]:
+    values,head,pointer=[],None,None
+    for l in lists:
+        while l:
+            heappush(values,l.val)
+            l=l.next
+
+    while values:
+        if head is None:
+            head=ListNode(heappop(values))
+            pointer=head
+
+        else:
+            pointer.next=ListNode(heappop(values))
+            pointer=pointer.next
+
+    return head
